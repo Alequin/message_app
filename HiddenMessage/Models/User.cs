df45fd3b453d13605c56ce14ad1088393bef511a
James Cox <j-cox03@hotmail.com>
2017-09-02 11:01:43 +0100

    Made user model constructors and made json method to return json string

--- HiddenMessage/Models/User.cs
+++ HiddenMessage/Models/User.cs
@@ -1,18 +1,27 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using HiddenMessage.Service.ServerRequests;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Reflection;
+
 namespace HiddenMessage.Models
 {
     public class User
     {
 
-        private String name;
-        private int avatar;
-        private String deviceSystem;
-        private String deviceToken;
-        private String onlineStatus;
-        private bool isVisible;
+        public String name;
+        public int avatar;
+        public String deviceSystem;
+        public String deviceToken;
+        public String onlineStatus;
+        public bool isVisible;
 
-        public User(String name, int avatar, String deviceSystem, String deviceToken, String onlineStatus, bool isVisible)
+        public User(string name, int avatar, string deviceSystem, string deviceToken, string onlineStatus, bool isVisible)
         {
+            System.Diagnostics.Debug.WriteLine("out: " + name + "--------------------------------------------------");
             this.name = name;
             this.avatar = avatar;
             this.deviceSystem = deviceSystem;
@@ -23,9 +32,36 @@ namespace HiddenMessage.Models
 
 		public User(String name, int avatar, String onlineStatus, bool isVisible)
 		: this(name, avatar, null, null, onlineStatus, isVisible){}
+
+
+        public String Name { get { return name; } }
+		public int Avatar { get { return avatar; } }
+        public String DeviceSystem { get  {return deviceSystem; } }
+		public String DeviceToken { get { return deviceToken; } }
+        public String OnlineStatus { get { return onlineStatus; } }
+		public bool IsVisible { get { return isVisible; } }
+
+        public void Save()
+        {
+            Dictionary<string, Object> jsonHash = new Dictionary<string, Object>();
+			jsonHash.Add("name", this.Name);
+			jsonHash.Add("avatar", this.Avatar);
+			jsonHash.Add("deviceSystem", this.DeviceSystem);
+			jsonHash.Add("deviceToken", this.DeviceToken);
+			jsonHash.Add("onlineStatus", this.OnlineStatus);
+			jsonHash.Add("isVisible", this.IsVisible);
+
+            Dictionary<string, Dictionary<string, object>> jsonContainerHash = new Dictionary<string, Dictionary<string, object>>();
+            jsonContainerHash.Add("User", jsonHash);
+
+            string jsonString = JsonConvert.SerializeObject(jsonContainerHash);
+
+            System.Diagnostics.Debug.WriteLine(jsonString + "----------------------------------------------");
+            //HttpRequest.MakePostRequest(ServerVariables.SERVER_URL, + "/users", null);
+        }
     }
 }
-
+ 
 
 
 //function User(name, avatar, deviceSystem, deviceToken, onlineStatus, isVisible)
