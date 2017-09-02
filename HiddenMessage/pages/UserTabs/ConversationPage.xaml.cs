a95e0f61665db74cc17d0b86457bfd131f049151
James Cox <j-cox03@hotmail.com>
2017-09-02 16:20:25 +0100

    Made other tabbed pages

---
+++ HiddenMessage/pages/UserTabs/ConversationPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HiddenMessage.pages.UserTabs
+{
+    public partial class ConversationPage : ContentPage
+    {
+        public ConversationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HiddenMessage/pages/UserTabs/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HiddenMessage.pages.UserTabs
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
