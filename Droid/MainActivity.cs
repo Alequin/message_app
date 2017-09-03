1499ef8e70d8dd2ca28026baeedb63a16b156b98
James Cox <j-cox03@hotmail.com>
2017-09-03 12:40:46 +0100

    Added in basic set up for firebase messaging (not got token output yet)

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -8,11 +8,19 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Android.Gms.Common;
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
+
 namespace HiddenMessage.Droid
 {
     [Activity(Label = "HiddenMessage.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+
+        private string TAG = "Android Logging: ";
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -20,9 +28,33 @@ namespace HiddenMessage.Droid
 
             base.OnCreate(bundle);
 
+            IsPlayServicesAvailable();
+
             global::Xamarin.Forms.Forms.Init(this, bundle);
 
             LoadApplication(new App());
+
+			
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    Log.Debug(TAG, GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    Log.Debug(TAG, "This device is not supported");
+                }
+                return false;
+            }
+            else
+            {
+                Log.Debug(TAG, "Google Play Services is available.");
+                return true;
+            }
         }
     }
 }
---
+++ HiddenMessage/Notifications/FireBaseTokenGetter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FCMClient;
+using Firebase.Iid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(FireBaseTokenGetter))]
+namespace HiddenMessage.Notifications
+{
+    public class FireBaseTokenGetter : IToken
+    {
+        public FireBaseTokenGetter()
+        {
+        }
+
+        public string GetToken()
+        {
+            return FirebaseInstanceId.Instance.Token;
+        }
+    }
+}
