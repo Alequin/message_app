29039fb19716b6d9bd168d025029931429f7fe35
James Cox <j-cox03@hotmail.com>
2017-09-03 11:15:17 +0100

    styled new user page and bound to NewUserViewModel object

--- HiddenMessage/pages/NewUserPage.xaml.cs
+++ HiddenMessage/pages/NewUserPage.xaml.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using HiddenMessage.ViewModels;
 using Xamarin.Forms;
 
 namespace HiddenMessage.pages
 {
     public partial class NewUserPage : ContentPage
     {
+
+        private NewUserViewModel newUserViewModel;
+
         public NewUserPage()
         {
             InitializeComponent();
+
+			newUserViewModel = new NewUserViewModel();
+			BindingContext = newUserViewModel;
+        }
+
+        void OnClickGoButton(object sender, System.EventArgs e)
+        {
+            newUserViewModel.setInstructionTextToWarningMessage();
         }
     }
 }
