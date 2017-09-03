e25444a139e47d4b9fb8662eb42ed411f58d3510
James Cox <j-cox03@hotmail.com>
2017-09-03 10:17:44 +0100

    Made blank users page

--- HiddenMessage/pages/UserTabs/UsersPage.xaml.cs
+++ HiddenMessage/pages/UserTabs/UsersPage.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using HiddenMessage.Models;
+using HiddenMessage.Service;
 using HiddenMessage.Service.ServerRequests;
 using Xamarin.Forms;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Converters;
 using System.Dynamic;
+using System.Collections.ObjectModel;
+using HiddenMessage.ViewModels;
 
 namespace HiddenMessage.pages.UserTabs
 {
@@ -18,9 +21,18 @@ namespace HiddenMessage.pages.UserTabs
 
 
             ListView lv = (ListView)list;
+            ObservableCollection<UserListViewModel> users = new ObservableCollection<UserListViewModel>();
+            lv.ItemsSource = users;
 
             //HttpRequest.MakeGetRequest(ServerVariables.URL + "/users", (content) => {
-                
+            //    JObject[] usersAsJson = JsonHelper.DeserialiseArray(content);
+
+            //    for (int j = 0; j < usersAsJson.Length; j++)
+            //    {
+            //        User user = new User(usersAsJson[j]);
+            //        users.Add(new UserListViewModel(user));
+            //    }
+            //    return null;
             //});
         }
     }
