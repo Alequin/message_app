8a9e0cfba97eda6f6a038c3911431f0b77533c12
James Cox <j-cox03@hotmail.com>
2017-09-04 09:50:21 +0100

    changed test for convo view model to use List

--- Specs/TestConversationListViewModel.cs
+++ Specs/TestConversationListViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using HiddenMessage.ViewModels;
 using NUnit.Framework;
 
@@ -8,17 +9,33 @@ namespace Specs
     [TestFixture]
     public class TestConversationListViewModel
     {
+
+        private List<String> userNames;
+        [SetUp]
+        public void Setup()
+        {
+			userNames = new List<string>() { "name1", "name2", "name3", "name4" };
+		}
+
 		[Test]
 		public void CanBuildStringOfUserNames()
 		{
-            String[] userNames = new string[] { "name1", "name2", "name3", "name4" };
-
             ConversationListViewModel viewModel = new ConversationListViewModel("last message", userNames);
 
             string expected = "Users: name1, name2, name3, name4";
             string result = viewModel.UsersText;
 		    Assert.AreEqual(expected, result);
 		}
+
+		//[Test]
+		//public void CanAddUser()
+		//{
+		//	ConversationListViewModel viewModel = new ConversationListViewModel("last message", userNames);
+
+		//	string expected = "Users: name1, name2, name3, name4";
+		//	string result = viewModel.UsersText;
+		//	Assert.AreEqual(expected, result);
+		//}
 
 	}
 }
