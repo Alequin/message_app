f5a6fd5428fc237b1e1090f647f8f9da84e228d5
James Cox <j-cox03@hotmail.com>
2017-09-04 14:34:31 +0100

    written code to produce list of conversations

--- HiddenMessage/pages/UserTabs/ConversationPage.xaml.cs
+++ HiddenMessage/pages/UserTabs/ConversationPage.xaml.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
+using HiddenMessage.Service;
+using HiddenMessage.Service.ServerRequests;
+using HiddenMessage.ViewModels;
+using Newtonsoft.Json.Linq;
 using Xamarin.Forms;
 
 namespace HiddenMessage.pages.UserTabs
 {
     public partial class ConversationPage : ContentPage
     {
+
+        UserSettings settings = new UserSettings();
+
         public ConversationPage()
         {
             InitializeComponent();
+
+			ListView listView = (ListView)list;
+			ObservableCollection<ConversationListViewModel> convos = new ObservableCollection<ConversationListViewModel>();
+			listView.ItemsSource = convos;
+
+            //String route = "/conversations/participants/user/" + settings.UserId.ToString();
+			//HttpRequest.MakeGetRequest(ServerVariables.URL + route, (content) => {
+			//	JObject[] convosAsJson = JsonHelper.DeserialiseArray(content);
+
+			//	return null;
+			//});
 		}
     }
 }
