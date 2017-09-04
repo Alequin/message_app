9668a16c9a21b2a5453ca104fe54b49fc6f97edc
James Cox <j-cox03@hotmail.com>
2017-09-04 09:47:26 +0100

    Made View Model for conversation list elements and made test for building users text list

---
+++ HiddenMessage/ViewModels/ConversationListViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace HiddenMessage.ViewModels
+{
+    public class ConversationListViewModel
+    {
+
+        private String[] usersCollection;
+        private String usersText;
+        private String lastMessage;
+
+        public ConversationListViewModel(String lastMessage, String[] usersCollection)
+        {
+            this.lastMessage = lastMessage;
+            this.usersCollection = usersCollection;
+            this.usersText = BuildUsersText();
+        }
+
+        public String UsersText
+        {
+            get { return "Users: " + usersText; }
+        }
+
+        private String BuildUsersText(){
+
+            string text = "";
+            int length = usersCollection.Length;
+            for (int j = 0; j < length; j++)
+            {
+                String user = usersCollection[j];
+                text += user;
+                if (j < length - 1) text += ", ";   
+            }
+
+            return text;
+        }
+    }
+}
---
+++ Specs/TestConversationListViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using HiddenMessage.ViewModels;
+using NUnit.Framework;
+
+
+namespace Specs
+{
+    [TestFixture]
+    public class TestConversationListViewModel
+    {
+		[Test]
+		public void CanBuildStringOfUserNames()
+		{
+            String[] userNames = new string[] { "name1", "name2", "name3", "name4" };
+
+            ConversationListViewModel viewModel = new ConversationListViewModel("last message", userNames);
+
+            string expected = "Users: name1, name2, name3, name4";
+            string result = viewModel.UsersText;
+		    Assert.AreEqual(expected, result);
+		}
+
+	}
+}
