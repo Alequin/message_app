ee5fceb119d84b812a09d3bb547751db0d14218f
James Cox <j-cox03@hotmail.com>
2017-09-05 11:14:02 +0100

    Added method to manage the send buttons actions and send the current message

--- HiddenMessage/pages/MessagePage.xaml.cs
+++ HiddenMessage/pages/MessagePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using HiddenMessage.Models;
 using HiddenMessage.Service;
 using HiddenMessage.Service.ServerRequests;
 using HiddenMessage.ViewModels;
@@ -13,6 +14,7 @@ namespace HiddenMessage.pages
     {
 
         private int conversationId;
+        private UserSettings settings = new UserSettings();
 
         public MessagePage(int conversationId)
         {
@@ -22,15 +24,24 @@ namespace HiddenMessage.pages
 
             ObservableCollection<MessageViewModel> messages = new ObservableCollection<MessageViewModel>();
 
-            //string route = "/messages/conversation/" + conversationId;
-            string route = "/messages/conversation/5";
+            string route = "/messages/conversation/" + conversationId;
+
 			HttpRequest.MakeGetRequest(ServerVariables.URL + route, (result) => {
                 JObject[] messagesAsJson = JsonHelper.DeserialiseArray(result);
 
-                System.Diagnostics.Debug.WriteLine(messagesAsJson);
-
+                foreach(JObject jsonMessage in messagesAsJson)
+                {
+                    System.Diagnostics.Debug.WriteLine("Out:" + jsonMessage);  
+                }
                 return null;
             }); 
+        }
+
+        void OnClickSendButton(object sender, System.EventArgs e)
+        {
+            string messageBody = ((Entry)sender).Text;
+            Message message = new Message(messageBody, settings.UserId, this.conversationId, null);
+            message.Save(null);
         }
     }
 }
