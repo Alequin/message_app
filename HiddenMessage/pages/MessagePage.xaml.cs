375617c6fc1bce12215054512b802c9a1568c01f
James Cox <j-cox03@hotmail.com>
2017-09-05 10:41:26 +0100

    Made basic outlines of messages page

--- HiddenMessage/pages/MessagePage.xaml.cs
+++ HiddenMessage/pages/MessagePage.xaml.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
+using HiddenMessage.Service;
+using HiddenMessage.Service.ServerRequests;
+using HiddenMessage.ViewModels;
+using Newtonsoft.Json.Linq;
 using Xamarin.Forms;
 
 namespace HiddenMessage.pages
 {
     public partial class MessagePage : ContentPage
     {
-        public MessagePage()
+
+        private int conversationId;
+
+        public MessagePage(int conversationId)
         {
             InitializeComponent();
+
+            this.conversationId = conversationId;
+
+            ObservableCollection<MessageViewModel> messages = new ObservableCollection<MessageViewModel>();
+
+            //string route = "/messages/conversation/" + conversationId;
+            string route = "/messages/conversation/5";
+			HttpRequest.MakeGetRequest(ServerVariables.URL + route, (result) => {
+                JObject[] messagesAsJson = JsonHelper.DeserialiseArray(result);
+
+                System.Diagnostics.Debug.WriteLine(messagesAsJson);
+
+                return null;
+            }); 
         }
     }
 }
