9de3742c13bb6de43210ffde9b5da0e594e8c946
James Cox <j-cox03@hotmail.com>
2017-09-05 13:54:03 +0100

    Made thread for updating messages and made it stop running when page is closed

--- HiddenMessage/pages/MessagePage.xaml.cs
+++ HiddenMessage/pages/MessagePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using HiddenMessage.Models;
 using HiddenMessage.Service;
 using HiddenMessage.Service.ServerRequests;
@@ -13,8 +14,12 @@ namespace HiddenMessage.pages
     public partial class MessagePage : ContentPage
     {
 
+        private Task updateTask;
+        private bool runUpdate;
         private int conversationId;
         private UserSettings settings = new UserSettings();
+
+        ObservableCollection<MessageViewModel> messages;
 
         public MessagePage(int conversationId)
         {
@@ -22,20 +27,35 @@ namespace HiddenMessage.pages
 
             this.conversationId = conversationId;
 
-            ObservableCollection<MessageViewModel> messages = new ObservableCollection<MessageViewModel>();
+            messages = new ObservableCollection<MessageViewModel>();
             list.ItemsSource = messages;
 
+			this.UpdateMessages();
+
+            runUpdate = true;
+			updateTask = new Task(async () => {
+				while (runUpdate)
+				{
+					this.UpdateMessages();
+					await Task.Delay(10000);
+				}
+			});
+            updateTask.Start();
+        }
+
+        private void UpdateMessages()
+        {
             string route = "/messages/conversation/" + conversationId;
-
 			HttpRequest.MakeGetRequest(ServerVariables.URL + route, (result) => {
-                JObject[] messagesAsJson = JsonHelper.DeserialiseArray(result);
+				JObject[] messagesAsJson = JsonHelper.DeserialiseArray(result);
 
-                foreach(JObject jsonMessage in messagesAsJson)
-                {
-                    messages.Add(new MessageViewModel(jsonMessage)); 
-                }
-                return null;
-            }); 
+                messages.Clear();
+				foreach (JObject jsonMessage in messagesAsJson)
+				{
+					messages.Add(new MessageViewModel(jsonMessage));
+				}
+				return null;
+			});
         }
 
         void OnClickSendButton(object sender, System.EventArgs e)
@@ -46,5 +66,11 @@ namespace HiddenMessage.pages
             message.Save(null);
             messageEntry.Text = "";
         }
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+            runUpdate = false;
+		}
     }
 }
