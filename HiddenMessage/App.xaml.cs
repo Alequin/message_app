3cdb9f135b76d350a511b5f595c2cc7de5dce820
James Cox <j-cox03@hotmail.com>
2017-09-06 11:09:37 +0100

    Added put requests to app.js to manage online and offline status for user

--- HiddenMessage/App.xaml.cs
+++ HiddenMessage/App.xaml.cs
@@ -4,16 +4,22 @@ using HiddenMessage.pages;
 using HiddenMessage.Service;
 using System.Globalization;
 using System;
+using HiddenMessage.Service.ServerRequests;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace HiddenMessage
 {
     public partial class App : Application
     {
+
+        private UserSettings settings;
+
         public App()
         {
             InitializeComponent();
 
-            UserSettings settings = new UserSettings();
+            settings = new UserSettings();
             if(!settings.IsUserSaved())
             {
                 MainPage = new NewUserPage();
@@ -25,16 +31,49 @@ namespace HiddenMessage
         protected override void OnStart()
         {
             System.Diagnostics.Debug.WriteLine("xoxostart");
+            if (settings.IsUserSaved())
+            {
+                System.Diagnostics.Debug.WriteLine("xoxostart: set online");
+				string route = "/users/online_status/" + settings.UserId;
+
+                Dictionary<string, string> onlineStatusHash = new Dictionary<string, string>()
+                { {"onlineStatus", "online"} };
+                string json = JsonConvert.SerializeObject(onlineStatusHash);
+
+                HttpRequest.MakePutRequest(ServerVariables.URL + route, json, null);
+            }
         }
 
         protected override void OnSleep()
         {
-			System.Diagnostics.Debug.WriteLine("xoxosleep");
+			System.Diagnostics.Debug.WriteLine("xoxosleep"); 
+            if (settings.IsUserSaved())
+			{
+                System.Diagnostics.Debug.WriteLine("xoxosleep: set offline");
+				string route = "/users/online_status/" + settings.UserId;
+
+				Dictionary<string, string> onlineStatusHash = new Dictionary<string, string>()
+				{ {"onlineStatus", "offline"} };
+				string json = JsonConvert.SerializeObject(onlineStatusHash);
+
+				HttpRequest.MakePutRequest(ServerVariables.URL + route, json, null);
+			}
 		}
 
         protected override void OnResume()
         {
 			System.Diagnostics.Debug.WriteLine("xoxoresume");
+			if (settings.IsUserSaved())
+			{
+                System.Diagnostics.Debug.WriteLine("xoxoresume: set online");
+				string route = "/users/online_status/" + settings.UserId;
+
+				Dictionary<string, string> onlineStatusHash = new Dictionary<string, string>()
+				{ {"onlineStatus", "online"} };
+				string json = JsonConvert.SerializeObject(onlineStatusHash);
+
+				HttpRequest.MakePutRequest(ServerVariables.URL + route, json, null);
+			}
 
 		}
     }
