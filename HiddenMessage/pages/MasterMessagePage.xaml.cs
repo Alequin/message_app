81c4449fcbc7b548c7141634f084659b2131a759
James Cox <j-cox03@hotmail.com>
2017-09-06 14:00:37 +0100

    MasterDetailPage.cs: Added code for click events

--- HiddenMessage/pages/MasterMessagePage.xaml.cs
+++ HiddenMessage/pages/MasterMessagePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using HiddenMessage.pages.UserTabs;
+using HiddenMessage.ViewModels;
 using Xamarin.Forms;
 
 namespace HiddenMessage.pages
@@ -14,6 +15,28 @@ namespace HiddenMessage.pages
             InitializeComponent();
             this.conversationId = conversationId;
             Detail = new MessagePage(conversationId);
+        }
+
+        void OnClickToMessagesButton(object sender, System.EventArgs e)
+		{
+			Detail = new MessagePage(conversationId);
+        }
+
+        void OnClickToInviteButton(object sender, System.EventArgs e)
+        {
+            UsersPage usersPage = new UsersPage();
+            Detail = usersPage;
+
+			usersPage.ListView.ItemTapped += async (listView, elements) =>
+			{
+				UserListViewModel selectedViewModel = (UserListViewModel)elements.Item;
+                bool answer = await DisplayAlert("", $"Invite {selectedViewModel.Name} to the conversation?", "Yes", "No");
+				if (answer)
+				{
+				    	
+				}
+				((ListView)listView).SelectedItem = null;
+			};
         }
     }
 }
